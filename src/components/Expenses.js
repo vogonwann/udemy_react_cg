5a9b79273a257d738574bd73d62b894840056c3c
vogonwann <wannjanjic@gmail.com>
2023-05-12 12:06:47 +0200

    add CSS

---
+++ src/components/Expenses.js
@@ -0,0 +1,27 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const expenses = props.expenses;
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
