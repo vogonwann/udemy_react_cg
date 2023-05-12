08311fa8dace543ddc14e212cbeb9ac114e1ab6a
vogonwann <wannjanjic@gmail.com>
2023-05-12 12:49:26 +0200

    add card container component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,11 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css";
 
 function Expenses(props) {
   const expenses = props.expenses;
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
@@ -20,7 +21,7 @@ function Expenses(props) {
         amount={expenses[2].amount}
         date={expenses[2].date}
       />
-    </div>
+    </Card>
   );
 }
 
