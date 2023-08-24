a35f2927ebfc53d00474ff131171c4bdc2b05b52
Ivan Janjić <wannjanjic@gmail.com>
2023-08-24 22:46:12 +0200

    assignement 3

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,32 +6,38 @@ import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
 const Expenses = (props) => {
   const [filter, setFilter] = useState("2020");
+  const [filteredExpenses, setFilteredExpenses] = useState([...props.expenses]);
+
+  let filterInfoText = "2019, 2021 & 2022";
+  if (filter === "2019") {
+    filterInfoText = "2020, 2021 & 2022";
+  } else if (filter === "2021") {
+    filterInfoText = "2019, 2020 & 2022";
+  } else if (filter === "2022") {
+    filterInfoText = "2019, 2020 & 2021";
+  }
 
   const expenses = props.expenses;
 
   const setFilterHandler = (filter) => {
     setFilter(filter);
-    console.log(filter);
+    console.log(filteredExpenses);
   };
   return (
     <>
-      <ExpensesFilter onSetFilterHandler={setFilterHandler} />
       <Card className="expenses">
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
+        <ExpensesFilter onSetFilterHandler={setFilterHandler} />
+        Excluded data for: {filterInfoText}
+        {[...props.expenses]
+          .filter((e) => e.date.getFullYear().toString() === filter)
+          .map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
       </Card>
     </>
   );
