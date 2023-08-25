ec1ad0143632dd6d7fea35c564fe2cfde8a9260d
Ivan Janjić <wannjanjic@gmail.com>
2023-08-25 23:06:02 +0200

    assignment-4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,43 +1,27 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filter, setFilter] = useState("2020");
   const [filteredExpenses, setFilteredExpenses] = useState([...props.expenses]);
-
-  let filterInfoText = "2019, 2021 & 2022";
-  if (filter === "2019") {
-    filterInfoText = "2020, 2021 & 2022";
-  } else if (filter === "2021") {
-    filterInfoText = "2019, 2020 & 2022";
-  } else if (filter === "2022") {
-    filterInfoText = "2019, 2020 & 2021";
-  }
 
   const expenses = props.expenses;
 
   const setFilterHandler = (filter) => {
     setFilter(filter);
     console.log(filteredExpenses);
+    setFilteredExpenses(
+      expenses.filter((e) => e.date.getFullYear().toString() === filter)
+    );
   };
   return (
     <>
       <Card className="expenses">
         <ExpensesFilter onSetFilterHandler={setFilterHandler} />
-        Excluded data for: {filterInfoText}
-        {[...props.expenses]
-          .filter((e) => e.date.getFullYear().toString() === filter)
-          .map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -10,9 +12,21 @@ const NewExpense = (props) => {
     };
     props.onAddExpense(expenseData);
   };
+
+  const cancelExpenseDataHandler = () => {
+    setShowForm(false);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpanseData={saveExpenseDataHandler} />
+      {!showForm && (
+        <button onClick={() => setShowForm(true)}>Add New Expense</button>
+      )}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpanseData={saveExpenseDataHandler}
+          onCancelExpense={cancelExpenseDataHandler}
+        />
+      )}
     </div>
   );
 };
